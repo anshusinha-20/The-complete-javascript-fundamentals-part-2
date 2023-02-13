ae437c88968d1bbcc88eadc9c860173f53f0ced8
Anshu Sinha <anshujuly2@gmail.com>
2023-02-13 16:27:09 +0530

    Add looping backwards and loops in loops assignment codes

--- script.js
+++ script.js
@@ -538,3 +538,20 @@
 
 // console.log(myCountry);
 
+/////
+
+// 48. Looping backwards and loops in loops
+
+// Assignment
+
+// 1. Storethisarrayofarraysintoavariablecalled'listOfNeighbours' [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+// 2. Logonlytheneighbouringcountriestotheconsole,onebyone,nottheentire arrays. Log a string like 'Neighbour: Canada' for each country
+// 3. Youwillneedaloopinsidealoopforthis.Thisisactuallyabittricky,sodon't worry if it's too difficult for you! But you can still try to figure this out anyway 😉
+
+// const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+
+// for (let i = 0; i < listOfNeighbours.length; i++) {
+//     for (let j = 0; j < listOfNeighbours[i].length; j++) {
+//         console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+//     }
+// }
